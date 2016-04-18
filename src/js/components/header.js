0f5ede716713af8658afe8bc186c76e8ec3bc9f5
Chris Laughlin <bluntm@gmail.com>
2016-04-18 22:15:59 +0100

    change image size on scroll

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -2,16 +2,37 @@ import React from 'react';
 import rowWithCentreContent  from 'styles/rowWithCentreContent';
 
 export default class extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {image: 'grow'};
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = (event) => {
+        let scrollTop = event.srcElement.body.scrollTop;
+
+        console.log('ScrollTop: ', scrollTop);
+        this.setState({image: scrollTop > 166 ? 'shrink' : 'grow'});
+    }
+
     render() {
         const bannerStyles = {
-            height: '130px'
+            height: this.state.image === 'grow' ? '130px' : '30px'
         };
         return (
             <div>
                 <div style={rowWithCentreContent}>
-                    <img style={bannerStyles} src='images/banner.jpg' />
+                    <img ref='headerImage' style={bannerStyles} src='images/banner.jpg' />
                 </div>
-                <div style={rowWithCentreContent}>
+                <div ref='subText' style={rowWithCentreContent}>
                     <span>The Dyslexic Developer</span>
                 </div>
             </div>
