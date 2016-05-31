794d0c12133e6c94062c4d459054f814ec578e4a
Chris Laughlin <bluntm@gmail.com>
2016-05-31 21:54:16 +0100

    adding goggle stats

--- src/js/module.js
+++ src/js/module.js
@@ -4,12 +4,20 @@ import React from  'react';
 import '../scss/main.scss';
 import { render } from 'react-dom'
 import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import ga from 'react-ga';
+//Components
 import Main from 'components/main';
 import SideBar from 'components/sideBar';
 import Post from 'components/post';
 import About from 'components/about';
 import Projects from 'components/projects';
 import Content from 'components/content';
+
+ga.initialize('UA-78587148-1');
+
+hashHistory.listen(location => {
+    ga.pageview(location.pathname);
+});
 
 render((
     <div id='outer-container'>
