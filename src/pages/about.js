c5d889c60a943196097a636b7b45a08b9440c8ba
Chris Laughlin <chris_laughlin@rapid7.com>
2020-04-18 10:15:07 +0100

    adding bio text

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,21 @@ import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import styled, { css } from 'styled-components';
+
+const baseHighlightStyles = css`
+  font-style: italic;
+  color: #C94660;
+`;
+
+const StyledWhoQuestion = styled.span`
+    font-size: 20px;
+    ${baseHighlightStyles}
+`;
+
+const StyledHighLightedText = styled.span`
+  ${baseHighlightStyles}
+`;
 
 const About = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -11,7 +26,22 @@ const About = ({ data, location }) => {
       <h1>
         About Me
       </h1>
-      Test
+      <section>
+        <p>
+          <StyledWhoQuestion>Who is the Dyslexic Developer?</StyledWhoQuestion> a great question that one one has asked.
+          I still feel the need to explain.
+        </p>
+        <p>
+          The Dyslexic Developer is <StyledHighLightedText>Chris Laughlin</StyledHighLightedText>, a software developer
+          from <StyledHighLightedText> Northern Ireland </StyledHighLightedText>. He started his journey in the crazy world
+          of development in 2010. Working with companies like <StyledHighLightedText> Microsoft </StyledHighLightedText>,
+          <StyledHighLightedText> Asidua </StyledHighLightedText>, and <StyledHighLightedText> Rapid7 </StyledHighLightedText>.
+          He spends way to much time on <StyledHighLightedText>Twitter </StyledHighLightedText> and <StyledHighLightedText> Github </StyledHighLightedText>
+          trying to maintain some sort of open-source presence. He has a <StyledHighLightedText>TODO </StyledHighLightedText>
+          list a mile long of things to try and learn that grows every day. Check out some of the adventures
+          he's taken below.
+        </p>
+      </section>
     </Layout>
   )
 }
