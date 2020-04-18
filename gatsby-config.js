6510befcb8a989935f819980d21d8da4797eba10
Chris Laughlin <chris_laughlin@rapid7.com>
2020-04-18 11:12:19 +0100

    adding anchor links

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,6 +80,7 @@ module.exports = {
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify`,
+    `gatsby-plugin-anchor-links`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
